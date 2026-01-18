ef049820e9891768f984dfaf5fc88284a6f51b3c
luceroeddy8@gmail.com <luceroeddy8@gmail.com>
2026-01-18 18:15:21 -0500

    archivos screen de cada vista creadas

---
+++ lib/screens/vaccine/vaccine_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VaccineScreen extends StatelessWidget {
+  const VaccineScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
